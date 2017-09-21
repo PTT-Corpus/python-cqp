6bc0bd8ec8a5baa5e6ce341eb98a2dcaeb258ece
amigcamel <amigcamel@gmail.com>
2017-09-22 00:02:58 +0800

    first commit

---
+++ pycqp/cqp.py
@@ -0,0 +1,121 @@
+# -*- coding: utf8 -*-
+"""A Python wrapper of PyCQP_interface."""
+import logging
+
+from CWB.CL import Corpus
+import PyCQP_interface
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class CQPClient:
+    """Wrap for `cqp` commandline utility.
+
+    :param: corpus: name of the corpus
+    """
+
+    def __init__(self, bin_path, corpus_name, registry_dir):
+        """Build `cqp` connection.
+
+        :bin_path: absolute path of `cqp` bin (usually `/usr/local/bin/cqp`)
+        :param: corpus_name: name of the corpus
+        :param: registry_dir: absolute path of `cqp` registry directory
+        """
+        cqp = PyCQP_interface.CQP(
+            bin=bin_path,
+            options=f'-c -r {registry_dir} -D {corpus_name}'
+        )
+        corpus = Corpus(corpus_name, registry_dir=registry_dir)
+        self.cqp = cqp
+        self.corpus = corpus
+
+    def __del__(self):
+        """Destroy `cqp` connection."""
+        try:
+            self.cqp.Terminate()
+        except Exception as e:
+            logger.warning(e)
+
+    @classmethod
+    def make_conc(
+            cls, query, page_num=1, num_per_page=50, show_pos=False,
+            begin_time=None, end_time=None, board_list=None, window_size=6):
+        """Make concordance."""
+        client = cls()
+        conclist = []
+
+        last = num_per_page * page_num - 1
+        first = last - num_per_page + 1
+
+        client.cqp.Exec('[word="%s"];' % query)
+
+        total = client.cqp.Exec("size Last;")
+        client.results = client.cqp.Dump(first=first, last=last)
+        if client.results == [['']]:
+            return None
+
+        words = client.corpus.attribute(b"word", "p")
+        if show_pos is True:
+            postags = client.corpus.attribute(b"pos", "p")
+        elif show_pos is False:
+            pass
+        else:
+            raise
+        boards = client.corpus.attribute(b"text_board", "s")
+        ptimes = client.corpus.attribute(b"text_time", "s")
+        for line in client.results:
+            output = dict()
+            start = int(line[0])
+            end = int(line[1]) + 1
+
+            # post_time filter
+            ptime = ptimes.find_pos(start)[-1]
+            if begin_time is not None and end_time is not None:
+                if begin_time <= int(ptime) <= end_time:
+                    pass
+                else:
+                    continue
+            elif begin_time is not None and end_time is None:
+                if int(ptime) < begin_time:
+                    continue
+
+            elif begin_time is None and end_time is not None:
+                if int(ptime) > end_time:
+                    continue
+
+            # board_list filter
+            board = boards.find_pos(start)[-1]
+
+            if board_list:
+                if board not in board_list:
+                    continue
+
+            lw = words[start - window_size:start]
+            rw = words[end:end + window_size]
+            qw = words[start:end]
+
+            if show_pos is True:
+                lp = postags[start - window_size:start]
+                rp = postags[end:end + window_size]
+                qp = postags[start:end]
+
+                left = ['%s/%s' % (word, pos) for word, pos in zip(lw, lp)]
+                mid = ['%s/%s' % (word, pos) for word, pos in zip(qw, qp)]
+                right = ['%s/%s' % (word, pos) for word, pos in zip(rw, rp)]
+
+            elif show_pos is False:
+                left = ' '.join(lw)
+                mid = ' '.join(qw)
+                right = ' '.join(rw)
+
+            output['conc'] = (left, mid, right)
+            output['board'] = board
+            output['time'] = ptime
+            conclist.append(output)
+        fin = {
+            'total': total,
+            'num_per_page': num_per_page,
+            'conclist': conclist
+        }
+        return fin
